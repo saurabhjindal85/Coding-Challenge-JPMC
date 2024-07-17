a5e5267236f27eddbc873d41bc50a2778089c8ef
saurabhjindal85 <srbhjindal85@gmail.com>
2024-07-17 19:30:37 +0800

    Adding javadocs and fixing issue with the method

--- src/main/java/com/jpmc/code/challenge/service/FindWord.java
+++ src/main/java/com/jpmc/code/challenge/service/FindWord.java
@@ -10,12 +10,27 @@ public class FindWord {
         this.character = character;
         this.statement = statement;
     }
+
+    /**
+     *Method return word having the most occurence of the search Character
+     *
+     * @return
+     */
     public String checkForCharacterInStmt(){
         String outputString  = Arrays.stream(this.statement.split(" "))
                 .filter(w -> w.contains(String.valueOf(this.character)))
                 .reduce("", (intermediate, newWord)-> checkForCharacterInStmt(intermediate, newWord));
-        return null;
+        return outputString;
     }
+
+    /**
+     *Method responible for checking the occurence of search character in each word
+     * compare it existing finding and returns the compared result
+     *
+     * @param intermediate
+     * @param newWord
+     * @return
+     */
 
     private String checkForCharacterInStmt(String intermediate, String newWord){
         String output = intermediate;
