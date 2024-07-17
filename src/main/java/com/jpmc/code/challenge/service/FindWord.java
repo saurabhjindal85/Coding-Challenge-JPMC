7d212f365796dd0e4ccc3e4d6603866e6531e8e6
saurabhjindal85 <srbhjindal85@gmail.com>
2024-07-17 19:24:09 +0800

    Creating logic for finding the word with most occurence of search character

--- src/main/java/com/jpmc/code/challenge/service/FindWord.java
+++ src/main/java/com/jpmc/code/challenge/service/FindWord.java
@@ -1,5 +1,8 @@
 package com.jpmc.code.challenge.service;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class FindWord {
     String statement;
     String character;
@@ -8,6 +11,23 @@ public class FindWord {
         this.statement = statement;
     }
     public String checkForCharacterInStmt(){
+        String outputString  = Arrays.stream(this.statement.split(" "))
+                .filter(w -> w.contains(String.valueOf(this.character)))
+                .reduce("", (intermediate, newWord)-> checkForCharacterInStmt(intermediate, newWord));
         return null;
+    }
+
+    private String checkForCharacterInStmt(String intermediate, String newWord){
+        String output = intermediate;
+        Long occInIntermediate = Arrays.stream(intermediate.split(""))
+                .filter(s-> s.equals(String.valueOf(character)))
+                .collect(Collectors.counting());
+        Long occInNewWord = Arrays.stream(newWord.split(""))
+                .filter(s-> s.equals(String.valueOf(character)))
+                .collect(Collectors.counting());
+
+        if(occInNewWord > occInIntermediate)
+            output = newWord;
+        return output;
     }
 }
